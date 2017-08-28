2bfb11298170577c8423c2602e77a6108cbe40cd
Mrugalla, Markus <M.Mrugalla@plan-net.com>
2017-08-28 13:06:45 +0200

    fix reset route path

--- src/app/index/env.js
+++ src/app/index/env.js
@@ -6,7 +6,7 @@ angular
       _authPath: 'https://bi.plan-net.com/api/v2',
       authPath: 'http://localhost:5050',
       basePath: 'http://localhost:5050',
-      noAuthRoutes: ['login', 'forgot-password', 'reset-password'],
+      noAuthRoutes: ['login', 'forgot-password', 'reset'],
       mainRoute: undefined,
       errors: {
         unknown: 'An unknown error occurred.'
