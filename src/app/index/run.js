f04c25e00ad5f83ae4d0613e2e3ed561a6ff9179
Mrugalla, Markus <M.Mrugalla@plan-net.com>
2017-09-04 10:41:20 +0200

    bump

--- src/app/index/run.js
+++ src/app/index/run.js
@@ -9,7 +9,7 @@ function run(BIAuthEnv, $log, $state, $rootScope, BIEvents, $mdDialog, $window) 
   }
 
   var unwatch1 = $rootScope.$on(BIEvents.UNAUTHORIZED, function () {
-    $state.go('login');
+    // $state.go('login');
     // $window.location.reload(true);
     $state.go('login', null, {
       notify: false
